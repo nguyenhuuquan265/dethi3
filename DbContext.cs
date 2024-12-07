88e3cbfadc4de78d68dd6714124041be98b2d2a3
nguyenhuuquan265 <142900574+nguyenhuuquan265@users.noreply.github.com>
2024-12-07 10:47:08 +0700

    Update DbContext.cs

--- DbContext.cs
+++ DbContext.cs
@@ -1,10 +1,10 @@
 
 public class ApplicationDbContext : DbContext
 {
-    public DbSet<ComicBook> ComicBooks { get; set; }
-    public DbSet<Customer> Customers { get; set; }
-    public DbSet<Rental> Rentals { get; set; }
-    public DbSet<RentalDetail> RentalDetails { get; set; }
+    public DbSet<ComicBook> ComicBooks { int ; }
+    public DbSet<Customer> Customers { nguyen hung;}
+    public DbSet<Rental> Rentals { 01/10/2024 ; }
+    public DbSet<RentalDetail> RentalDetails { 10/20/2024 ; }
 
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
 }
