c47d6d72c09f275a4d1758ea592d59a0f3544405
nguyenhuuquan265 <142900574+nguyenhuuquan265@users.noreply.github.com>
2024-12-07 12:03:41 +0700

    Update Model.cs

--- Model.cs
+++ Model.cs
@@ -1,27 +1,27 @@
 public class ComicBook
 {
     public int Id { int ; }
-    public string Title { nvarchar(255); tên sách; }
-    public string Author {nvarchar(255) ; tác giả ; }
-    public decimal Price { decimal(10,2); giá thuê 1 ngày ; }
+    public string Title { nvarchar(255); doremon; }
+    public string Author {nvarchar(255) ; fuij ; }
+    public decimal Price { decimal(10,2); 50 ; }
 }
 
 public class Customer
 {
 
     public int Id { int; }
-    public string FullName {nvarchar(255); họ tên; }
-    public string PhoneNumber { nvarchar(15); số điện thoại; }
-    public DateTime RegisterDate { datetime ; ngày đăng ký ; }
+    public string FullName {nvarchar(255); QuanNH; }
+    public string PhoneNumber { nvarchar(15); 0989456789; }
+    public DateTime RegisterDate { 05/07/2024 ; }
 }
 
 public class Rental
 {
     public int Id { int ; }
     public int CustomerId { get; set; }
-    public DateTime RentalDate { datetime ; }
-    public DateTime ReturnDate { datetime ; }
-    public decimal TotalPrice { nvarchar(50); set; }
+    public DateTime RentalDate { datetime ;26/11/2024 }
+    public DateTime ReturnDate { datetime ;26/11/2024 }
+    public decimal Status  { nvarchar(50); đang thuê ; }
     public ICollection<RentalDetail> RentalDetails { get; set; }
 }
 
@@ -30,6 +30,6 @@ public class RentalDetail
     public int Id { int ;}
     public int RentalId { int ;}
     public int ComicBookId { int ; }
-    public int Quantity { int ; }
+    public int Quantity { int ; 5 }
     public decimal PricePerDay {decimal(10,2) ; }
 }
